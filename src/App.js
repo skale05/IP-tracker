9aa0e3bb63c460697d7f8f1f841d40231155a642
Aleksandar <skale05@yahoo.com>
2023-06-05 15:03:18 +0200

    popup made

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import axios from 'axios';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 
-
-
 import 'leaflet/dist/leaflet.css';
 import './App.css';
-
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -45,60 +42,54 @@ function App() {
     }
   }, [ipInfo]);
 
-const defaultIcon = L.icon({
-  iconUrl: iconUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
-  shadowSize: [41, 41],
-});
+  const defaultIcon = L.icon({
+    iconUrl: iconUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+    shadowSize: [41, 41],
+  });
 
-L.Marker.prototype.options.icon = defaultIcon;
+  L.Marker.prototype.options.icon = defaultIcon;
 
-
-return (
-  <div className="App">
-    <h1>What is my IP</h1>
-    {ipInfo && (
-      <div>
-        <h2>Your IP Address: {ipInfo.ip}</h2>
-        {countryInfo && (
-          <div>
-            <h3>Country: {countryInfo.name.common}</h3>
-            <img src={countryInfo.flags.svg} alt="Flag" className="flag" />
-            {/* <p>Capital: {countryInfo.capital}</p>
-            <p>Population: {countryInfo.population}</p>
-            <p>Area: {countryInfo.area} km²</p>
-            <p>Languages: {Object.values(countryInfo.languages).join(", ")}</p> */}
-          </div>
-        )}
+  return (
+    <div className="App">
+      <h1>What is my IP</h1>
+      {ipInfo && (
         <MapContainer
           center={[ipInfo.latitude, ipInfo.longitude]}
-          zoom={6}
-          style={{ height: "400px", width: "100%" }}
+          zoom={12}
+          style={{ height: '600px', width: '100%' }}
         >
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <Marker position={[ipInfo.latitude, ipInfo.longitude]}>
-            <Popup>Your Location</Popup>
+            <Popup>
+              <div>
+                <h4>Your Location</h4>
+                <p>IP Address: {ipInfo.ip}</p>
+                <p>Location: {ipInfo.city}, {ipInfo.region}</p>
+                <p>Country: {countryInfo?.name?.common}</p>
+                {countryInfo && (
+                  <img src={countryInfo.flags.svg} alt="Flag" className="flag" />
+                )}
+              </div>
+              <div>
+                <h4>Your Local Time</h4>
+                <p>Date and Time: {DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}</p>
+                {ipInfo.timezone && (
+                  <>
+                    <h4>Time in {ipInfo.timezone}</h4>
+                    <p>Date and Time: {DateTime.now().setZone(ipInfo.timezone).toLocaleString(DateTime.DATETIME_FULL)}</p>
+                  </>
+                )}
+              </div>
+            </Popup>
           </Marker>
         </MapContainer>
+      )}
+    </div>
+  );
+}
 
-        <div>
-          <h3>Your Local Time</h3>
-          <p>Date and Time: {DateTime.now().toLocaleString(DateTime.DATETIME_FULL)}</p>
-          <p>Timezone: {DateTime.now().zoneName}</p>
-        </div>
-
-        {ipInfo.timezone && (
-          <div>
-            <h3>Time in {ipInfo.timezone}</h3>
-            <p>Date and Time: {DateTime.now().setZone(ipInfo.timezone).toLocaleString(DateTime.DATETIME_FULL)}</p>
-          </div>
-        )}
-      </div>
-    )}
-  </div>
-);
-        }
 export default App;
